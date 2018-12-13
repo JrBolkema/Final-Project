3440522c2e9ddfb9a699359ecdb9fa36593333bd
Jon Bolkema <jrbolkema@gmail.com>
2018-12-12 23:53:03 -0700

    changed a file name

---
+++ FinalProjectDatabase.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE Users (
+	UserID int PRIMARY KEY IDENTITY,
+	Username varchar(25),
+	Password varchar(25)
+	);
+
+CREATE TABLE Workouts(
+	UserID int FOREIGN KEY REFERENCES Users(UserID),
+	Date date DEFAULT(CONVERT(date,CURRENT_TIMESTAMP)),
+	PRIMARY KEY(UserID,Date),
+	WorkoutName varchar(25),
+	Sets int,
+	Reps int,
+	Weight int
+);
